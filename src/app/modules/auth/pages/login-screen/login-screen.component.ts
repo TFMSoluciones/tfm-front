3625c4ba4eba1bc6f3855d342cdbd5c7b5b095f8
Gustavo Romero <g.romeron2@uniandes.edu.co>
2023-01-01 19:59:38 -0500

    auth initial html before changes

--- src/app/modules/auth/pages/login-screen/login-screen.component.ts
+++ src/app/modules/auth/pages/login-screen/login-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login-screen',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-screen.component.css']
 })
 export class LoginScreenComponent implements OnInit {
+
+  formLogin: FormGroup = new FormGroup({});
 
   constructor() { }
 
