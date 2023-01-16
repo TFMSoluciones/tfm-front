b941080efc4312c5c5c1587ef88285e79f118e4f
Gustavo Romero <g.romeron2@uniandes.edu.co>
2023-01-16 16:59:10 -0500

    initial ci testing pipeline added v8

--- karma.conf.js
+++ karma.conf.js
@@ -37,14 +37,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    /* browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+    /* browsers: ['Chrome'], */
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
-    }, */
+    },
     /* browsers: ['Chrome', 'Chrome_without_security'], */ // You may use 'ChromeCanary', 'Chromium' or any other supported browser
 
     // you can define custom flags
